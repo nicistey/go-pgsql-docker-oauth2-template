107518ff4e777e431b0b7e303d363f735813fd3f
nicistey <nicisteylv@ya.ru>
2024-12-07 01:13:46 +0500

    Сделан docker-compose

--- Server/cmd/main.go
+++ Server/cmd/main.go
@@ -9,14 +9,17 @@ import (
 )
 
 
-const connStrDB = "postgres://nicis:123@localhost:5432/kudago"
+const connStrDB = "postgres://nicis:123@postgres:5432/kudago"
+//const connStrDB = "postgres://nicis:123@localhost:5432/kudago"
 
 func main() {
+	log.Println("START SERVER")
 	db,err := repository.New(connStrDB)
 	if(err!=nil){
 		log.Fatal(err.Error())
 	}
 	api:= api.New(mux.NewRouter(),db )
 	api.Hadle()
-	log.Fatal(api.ListenAndServe("localhost:8090"))
+	// log.Fatal(api.ListenAndServe("localhost:8090"))
+	log.Fatal(api.ListenAndServe("0.0.0.0:8090"))
 }
